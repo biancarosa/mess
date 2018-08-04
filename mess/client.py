3d4596de431ae222f16dc49b9aa631f225af07d6
Bianca Rosa <me@biancarosa.com.br>
2018-08-04 11:21:35 -0300

    run with asyncio

--- mess/client.py
+++ mess/client.py
@@ -1,22 +1,26 @@
 from datetime import datetime
-
+import sys
 import Pyro4
+import asyncio
 
 from config import SERVER_NAME
 
 server = Pyro4.Proxy(f"PYRONAME:{SERVER_NAME}")
 
-def start_chatting():
+async def start_chatting(loop):
     text = ''
     while (text != 'exit'):
-        text = input("... ")
+        text = await loop.run_in_executor(None, sys.stdin.readline)
+        text = text.strip()
         now = datetime.now()
         server.send_message(text)
         print(f'sent at {now:%H:%M:%S} \n')
 
 if __name__ == '__main__':
     try:
-        start_chatting()
+        loop = asyncio.get_event_loop()
+        loop.run_until_complete(start_chatting(loop))
+        loop.close()
     except (KeyboardInterrupt, EOFError):
         print('Goodbye! (:')
         exit
